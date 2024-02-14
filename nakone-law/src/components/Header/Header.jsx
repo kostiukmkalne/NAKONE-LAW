0af2bc66b64482b16969158f9ad86f7380df7ffd
Marian Kostiuk <131563162+kostiukmkalne@users.noreply.github.com>
2024-02-14 13:06:46 -0600

    change my header

--- nakone-law/src/components/Header/Header.jsx
+++ nakone-law/src/components/Header/Header.jsx
@@ -1,18 +1,20 @@
 import './Header.css';
 import logo from '../../assets/logo.svg';
 import Navigation from '../Navigation/Navigation';
+import Button from '../Button/Button';
 
 const Header = () => (
   <header className="header">
     <a href="#home">
       <img 
         src={logo} 
-        className="App-logo" 
+        className="app-logo" 
         alt="logo" 
       />
     </a>
 
     <Navigation />
+    <Button />
   </header>
 );
 
