09049819473bce756a2fa96324df59a7d2791c70
Marian Kostiuk <131563162+kostiukmkalne@users.noreply.github.com>
2024-04-19 13:35:00 -0500

    updated my header

--- nakone-law/src/components/Header/Header.jsx
+++ nakone-law/src/components/Header/Header.jsx
@@ -3,6 +3,9 @@ import logo from "../../assets/logo.svg";
 import Navigation from "../Navigation/Navigation";
 import Button from "../Button/Button";
 import arroww from "../../assets/arroww.svg";
+import eng from '../../assets/language/eng.svg';
+import ua from '../../assets/language/ua.svg';
+
 
 const Header = ({ phoneNumber, consultation }) => (
   <header className="header">
@@ -14,12 +17,23 @@ const Header = ({ phoneNumber, consultation }) => (
       847-391-1008
     </Button>
 
+    <Navigation className="navigation" />
+
     <Button href={consultation} className="consultation-header-button">
       CONSULTATION
       <img src={arroww} className="banner-button-arrow" alt="arrowb" />
     </Button>
 
-    <Navigation className="navigation" />
+    <div className="language-block">
+
+      <a href="#home">
+        <img src={eng} className="eng-language" alt="#eng" />
+      </a>
+
+      <a href="#home">
+        <img src={ua} className="ua-language" alt="#ua" />
+      </a>
+    </div>
   </header>
 );
 
