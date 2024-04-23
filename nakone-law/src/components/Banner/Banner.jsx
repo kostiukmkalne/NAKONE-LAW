0ab06b1e11a143e14a5b7bab8a71986f1b81f8ed
Marian Kostiuk <131563162+kostiukmkalne@users.noreply.github.com>
2024-04-22 22:54:34 -0500

    a litle bit fixe banner page

--- nakone-law/src/components/Banner/Banner.jsx
+++ nakone-law/src/components/Banner/Banner.jsx
@@ -2,6 +2,9 @@ import './Banner.css';
 import bannerlogo from '../../assets/bannerlogo.svg';
 import Button from '../Button/Button';
 import arrowb from "../../assets/arrowb.svg";
+import phone from '../../assets/phone.svg';
+import chat from '../../assets/chat.svg';
+
 
 const Banner = () => (
   <div className="banner">
@@ -17,6 +20,18 @@ const Banner = () => (
       CONSULTATION
       <img src={arrowb} className="banner-button-arrow" alt="arrowb" />
     </Button>
+
+    <div className="comunication-buttons">
+      <Button className="banner-button-phone">
+        <a href="tel:+1 (847)391-1008">
+          <img src={phone} className="phone-logo" alt="phone" />
+        </a>
+      </Button>
+
+      <Button className="banner-button-chat">
+        <img src={chat} className="chat-logo" alt="chat" />
+      </Button>
+    </div>
   </div>
 );
 
